cc160f87fcaed3ea73c58a4306c6768d446298fa
henriquelisboa2902 <henriquelisboa2902@gmail.com>
2025-04-16 18:18:16 +0000

    separação dos cards

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import { useState } from "react"
+import { CardInicial } from "./components/CardInicial"
+import { CardFinal } from "./components/CardFinal"
+
+export function App() {
+  const [submited, setSubmited] = useState(false)
+  const [rateNote, setRateNote] = useState(0)
+
+  function handleSubmit() {
+    if (rateNote !== 0) {
+      setSubmited(true)
+      return
+    }
+
+    alert("Please, choose a rate button!")
+  }
+
+  function handleChangeRateNote(value) {
+    console.log (value)
+    setRateNote(value)
+  }
+
+  return (
+
+    submited === false ? (
+      <CardInicial handleChangeRateNote={handleChangeRateNote} handleSubmit={handleSubmit}/>
+    ) : (
+      <CardFinal rateNote={rateNote}/>
+    )
+
+  )
+}
